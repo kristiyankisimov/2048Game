27b46c2c4d92c36b849b371fb30c41691072c3d3
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-07-10 19:00:45 +0300

    add some functions

--- src/com/hackbulgaria/java/Game.java
+++ src/com/hackbulgaria/java/Game.java
@@ -1,5 +1,33 @@
 package com.hackbulgaria.java;
 
 public class Game {
+    private int gameMatrix[][];
 
+    public Game() {
+
+    }
+
+    public void moveLeft() {
+
+    }
+
+    public void moveRight() {
+
+    }
+
+    public void moveUp() {
+
+    }
+
+    public void moveDown() {
+
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public boolean isWon() {
+        return false;
+    }
 }
